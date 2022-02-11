f293789d65738e99ec5b590f6d813fb60d26a1c9
akmalsyrf <dev.akmal69@gmail.com>
2022-02-11 10:42:35 +0700

    bank model, router, and controller

---
+++ app/bank/model.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+let bankSchema = mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "Nama pemilik harus diisi"],
+  },
+  nameBank: {
+    type: String,
+    required: [true, "Nama bank harus diisi"],
+  },
+  noRekening: {
+    type: String,
+    required: [true, "No rekening bank harus diisi"],
+  },
+});
+
+module.exports = mongoose.model("Bank", bankSchema);
